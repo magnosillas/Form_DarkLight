7bd2bf50e978bd5690d97b1191e1c35edaa4ab9f
Magno Sillas <magno.sillas0@gmail.com>
2023-01-27 05:15:08 -0300

    upload

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+const mode = document.getElementById('mode_icon');
+
+const form = document.getElementById('login_form');
+
+mode.addEventListener('click', () => {
+    if(mode.classList.contains('fa-moon')){
+        mode.classList.remove('fa-moon');
+        mode.classList.add('fa-sun');
+
+        form.classList.add('dark');
+
+        return;
+    }
+    mode.classList.add('fa-moon');
+    mode.classList.remove('fa-sun');
+    form.classList.remove('dark');
+
+
+
+});
